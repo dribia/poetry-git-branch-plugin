d447b8d99094498ba16b5ab7966c49ca1290d012
Francesc Font Clos <fran@dribia.com>
2023-12-21 18:02:05 +0100

    Reformat

--- poetry_git_branch_plugin/plugin.py
+++ poetry_git_branch_plugin/plugin.py
@@ -1,4 +1,3 @@
-
 import os
 
 from cleo.events.console_events import COMMAND
@@ -11,22 +10,16 @@ from poetry.plugins.application_plugin import ApplicationPlugin
 
 class PoetryGitBranchPlugin(ApplicationPlugin):
     def activate(self, application: Application):
-        application.event_dispatcher.add_listener(
-            COMMAND, self.set_git_branch_env_var
-        )
+        application.event_dispatcher.add_listener(COMMAND, self.set_git_branch_env_var)
 
     def set_git_branch_env_var(
-        self,
-        event: ConsoleCommandEvent,
-        event_name: str,
-        dispatcher: EventDispatcher
+        self, event: ConsoleCommandEvent, event_name: str, dispatcher: EventDispatcher
     ) -> None:
-
         if not isinstance(event.command, EnvCommand):
             return
 
         io = event.io
-        env_var = 'POETRY_GIT_BRANCH'
+        env_var = "POETRY_GIT_BRANCH"
         io.write_line(f"Setting {env_var} environment variable...")
         branch = os.popen("git symbolic-ref --short HEAD").read().strip()
         os.environ[env_var] = branch
