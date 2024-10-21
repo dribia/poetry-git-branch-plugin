468867c9f204e3ec5d701eeb9f583f81c3b9b748
Helena Ribera <helena@dribia.com>
2024-10-21 10:48:07 +0200

    Add tests for both version found and version not found.

--- tests/test_poetry_git_branch_plugin.py
+++ tests/test_poetry_git_branch_plugin.py
@@ -1,10 +1,33 @@
-"""Package test module."""
+"""Test the module initialization and version information."""
 
-import re
+import sys
+from importlib import reload
+from importlib.metadata import PackageNotFoundError
+from unittest.mock import patch
 
-import poetry_git_branch_plugin
 
+class TestInit:
+    """Test the module initialization and version information."""
 
-def test_version():
-    """Assert that `__version__` exists and is valid."""
-    assert re.match(r"\d.\d.\d", poetry_git_branch_plugin.__version__)
+    @patch("importlib.metadata.version", side_effect=PackageNotFoundError)
+    def test_version_not_found(self, mock_version):
+        """Test `__version__` when the package is not found."""
+        if "poetry_git_branch_plugin" in sys.modules:
+            del sys.modules["poetry_git_branch_plugin"]
+
+        import poetry_git_branch_plugin
+
+        reload(poetry_git_branch_plugin)
+        assert poetry_git_branch_plugin.__version__ == "unknown"
+
+    @patch("importlib.metadata.version")
+    def test_version_found(self, mock_version):
+        """Test `__version__` when the package is found."""
+        mock_version.return_value = "0.1.0"
+        if "poetry_git_branch_plugin" in sys.modules:
+            del sys.modules["poetry_git_branch_plugin"]
+
+        import poetry_git_branch_plugin
+
+        reload(poetry_git_branch_plugin)
+        assert poetry_git_branch_plugin.__version__ == "0.1.0"
